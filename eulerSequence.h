09c4483cd032b386e1ad9cf0f264eb2b775dd9d1
Arthur <arthurhdrodrigues@gmail.com>
2022-09-27 16:41:03 -0300

    add .gitignore and deleted main binary

--- eulerSequence.h
+++ eulerSequence.h
@@ -5,9 +5,15 @@
 #include "treaps.h"
 
 struct Treap eulerSeq( struct vertice from, struct vertice to);
+
 void printSeq(struct Treap* treap);
+
 struct Treap* find(struct Treap* node);
+
 struct Treap* slice(struct Treap** raiz, unsigned int K);
+
 struct Treap* concatenate(struct Treap* raizA, struct Treap* raizB);
+
 void bringToFront(struct Treap* node);
+
 #endif
