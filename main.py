d4785a850cfb91ed1208926a423b375a0b323610
a <arthurhdrodrigues@gamil.com>
2023-06-11 13:38:26 -0300

    add random test in main

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from dg  import *
 from ETT  import *
+from random import randint
 
 
+VNUM = 20
 def main():
-    G = dynamicGraph(100)
+    G = dynamicGraph(VNUM)
 
-    for i in range(1,100):
+    for i in range(1,VNUM):
         addEdge(G,i,i+1)
 
-    addEdge(G,5,20)
-    addEdge(G,35,50)
-    addEdge(G,78,3)
+    for i in range(10):
+        r = randint(1,VNUM-1)
+        s = randint(r+1,VNUM)
+        print(i,":(",r,",",s,")")
+        addEdge(G,r,s)
     printDG(G)
-    #printSequence(getRoot(G.F[G.maxLevel].H[(1,1)]))
-    remEdge(G,10,11)
+    print("===============")
+    for i in range(10):
+        r = randint(1,VNUM-1)
+        s = randint(r+1,VNUM)
+        while (r,s) not in G.F[G.maxLevel].H:
+            r = randint(1,VNUM-1)
+            s = randint(r+1,VNUM)
+        print(i,":(",r,",",s,")")
+        remEdge(G,r,s)
     printDG(G)
     #printSequence(getRoot(G.F[G.maxLevel].H[(1,1)]))
 main()
