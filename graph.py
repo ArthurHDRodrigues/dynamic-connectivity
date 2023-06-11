516b14b0d300b9a1ddf526241b0a039fbbda2c30
a <arthurhdrodrigues@gamil.com>
2023-06-11 13:37:54 -0300

    add __str__ for graph class

--- graph.py
+++ graph.py
@@ -1,8 +1,23 @@
 class graph:
     def __init__(self,n):
-        self.AL = []
+        self.AL = ['']
         for i in range(n):
             self.AL.append(set())
+
+    def __str__(self):
+        s = ''
+        for u in range(1,len(self.AL)):
+            if len(self.AL[u]) > 0:
+                s+=str(u)+":"
+                l = len(self.AL[u])
+                i = 0
+                for v in self.AL[u]:
+                    s+=str(v)
+                    i+=1
+                    if i<l:
+                        s+=','
+                s+='; '
+        return s
 
 def addEdgeGLA(G,u,v):
     G.AL[u].add(v)
@@ -11,3 +26,4 @@ def addEdgeGLA(G,u,v):
 def delEdgeGLA(G,u,v):
     G.AL[u].remove(v)
     G.AL[v].remove(u)
+
