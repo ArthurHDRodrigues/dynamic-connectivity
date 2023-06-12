2b611bb5f728cd4b73260c99af3c177edc713d30
Arthur Rodrigues <arthurhdrodrigues@gmail.com>
2023-06-11 21:21:37 -0300

    add support to reading files

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
 from dg  import *
-from ETT  import *
-from random import randint
+from readFile  import *
 
 
-VNUM = 20
 def main():
-    G = dynamicGraph(VNUM)
-
-    for i in range(1,VNUM):
-        addEdge(G,i,i+1)
-
-    for i in range(10):
-        r = randint(1,VNUM-1)
-        s = randint(r+1,VNUM)
-        print(i,":(",r,",",s,")")
-        addEdge(G,r,s)
-    printDG(G)
-    print("===============")
-    for i in range(10):
-        r = randint(1,VNUM-1)
-        s = randint(r+1,VNUM)
-        while (r,s) not in G.F[G.maxLevel].H:
-            r = randint(1,VNUM-1)
-            s = randint(r+1,VNUM)
-        print(i,":(",r,",",s,")")
-        remEdge(G,r,s)
-    printDG(G)
-    #printSequence(getRoot(G.F[G.maxLevel].H[(1,1)]))
+    importFromFile("test.txt",1)
 main()
---
+++ readFile.py
@@ -0,0 +1,34 @@
+from dg import *
+
+def importFromFile(fileName,VERBOSE=0):
+    file = open(fileName, 'r')
+    lines = file.readlines()
+    n = int(lines.pop(0))
+    G = dynamicGraph(n)
+    for line in lines:
+        symbol_list = line.split()
+        operation = symbol_list[0]
+        u = int(symbol_list[1])
+        v = int(symbol_list[2])
+        match operation:
+            case '+':
+                if VERBOSE:
+                    print("Adding edge (",u,",",v,")")
+                addEdge(G,u,v)
+            case '-':
+                if VERBOSE:
+                    print("Deleting edge (",u,",",v,")")
+                remEdge(G,u,v)
+            case 'q':
+                if connected(G,u,v):
+                    print("vertices ",u," and ",v," are connected")
+                else:
+                    print("vertices ",u," and ",v," are NOT connected")
+            case default:
+                print("Error, unknown operation")
+
+
+
+    return G
+
+
