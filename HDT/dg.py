290a6079eeef59cc70ed92aac78306c7b053c67c
Arthur Rodrigues <arthurhdrodrigues@gmail.com>
2024-06-16 16:04:45 -0300

    Add commented debug code in HDT/dg.py and fix it

--- HDT/dg.py
+++ HDT/dg.py
@@ -26,24 +26,45 @@ def printDG(G):
 def addTreeEdge(G, u, v):
     if (u,v) in G.level:
         return
+    #log(HDTlog,'add',' te', G.maxLevel,u,v)
 
     G.level[(u,v)] = G.maxLevel
     addEdgeDF(G.F[G.maxLevel],u,v)
 
+    #uu = G.F[G.maxLevel].H[(u,u)]
+    #sanityCheck(getRoot(uu),G.F[G.maxLevel])
+
+
 def addNonTreeEdge(G, u, v):
     if (u,v) in G.level:
         return
+    #log(HDTlog,'add','nte', G.maxLevel,u,v)
     G.level[(u,v)] = G.maxLevel
 
-    G.F[G.maxLevel].nte[u].add(v)
+    #uu = G.F[G.maxLevel].H[(u,u)]
+    #sanityCheck(getRoot(uu),G.F[G.maxLevel])
+
+    #vv = G.F[G.maxLevel].H[(v,v)]
+    #sanityCheck(getRoot(vv),G.F[G.maxLevel])
+
+    G.F[G.maxLevel].nte[u].add(v) 
     if len(G.F[G.maxLevel].nte[u]) == 1:
         uu = G.F[G.maxLevel].H[(u,u)]
         incrementReserveDegree(uu)
+        #sanityCheck(getRoot(uu),G.F[G.maxLevel])
     
     G.F[G.maxLevel].nte[v].add(u)
     if len(G.F[G.maxLevel].nte[v]) == 1:
         vv = G.F[G.maxLevel].H[(v,v)]
         incrementReserveDegree(vv)
+        #sanityCheck(getRoot(vv),G.F[G.maxLevel])
+
+    #uu = G.F[G.maxLevel].H[(u,u)]
+    #sanityCheck(getRoot(uu),G.F[G.maxLevel])
+
+    #vv = G.F[G.maxLevel].H[(v,v)]
+    #sanityCheck(getRoot(vv),G.F[G.maxLevel])
+    
 
 def addEdge(G,u,v):
     '''
@@ -94,9 +115,9 @@ def remTreeEdge(G, u, v):
     replace(G,u,v,level)
 
 def remNonTreeEdge(G,u,v):
-    #log(HDTlog,'del','nte', level,u,v)
     level = G.level[(u,v)]
     del(G.level[(u,v)])
+    #log(HDTlog,'del','nte', level,u,v)
     
     G.F[level].nte[u].remove(v)
     if len( G.F[level].nte[u] ) == 0:
@@ -108,12 +129,16 @@ def remNonTreeEdge(G,u,v):
         vv = G.F[level].H[(v,v)]
         decrementReserveDegree(vv)
 
-def remEdge(G,u,v):
+    #uu = G.F[level].H[(u,u)]
+    #sanityCheck(getRoot(uu),G.F[level])
+    #vv = G.F[level].H[(v,v)]
+    #sanityCheck(getRoot(vv),G.F[level])
+
+#def remEdge(G,u,v):
     '''
     It assumes that u < v.
 
     Runs in O(lg² n) expected amortized.
-    '''
     if (u,v) not in G.level:
         return
 
@@ -121,12 +146,12 @@ def remEdge(G,u,v):
     del(G.level[(u,v)])
     
     if (u,v) in G.F[G.maxLevel].H:
-        #log(HDTlog,'del',' te', level,u,v)
+        log(HDTlog,'del',' te', level,u,v)
         for i in range(level,G.maxLevel+1):
             remEdgeDF(G.F[i],u,v)
         replace(G,u,v,level)
     else:
-        #log(HDTlog,'del','nte', level,u,v)
+        log(HDTlog,'del','nte', level,u,v)
         uu = G.F[level].H[(u,u)]
         uu.nte.remove(v)
         decrementReserveDegree(uu)
@@ -134,6 +159,7 @@ def remEdge(G,u,v):
         vv = G.F[level].H[(v,v)]
         vv.nte.remove(u)
         decrementReserveDegree(vv)
+ '''
 
 
 def replace(G,u,v,level):
@@ -146,38 +172,60 @@ def replace(G,u,v,level):
     for i in range(level,G.maxLevel+1):
         if (u,u) not in G.F[i].H:
             if len(G.F[i].nte[u]) > 0:
-                xy = G.F[i].nte[u].pop()
-                x,y = xy.val
-                if x < y:
-                    a = x
+                y = G.F[i].nte[u].pop()
+                G.F[i].nte[y].remove(u)
+
+
+                if len( G.F[i].nte[y] ) == 0:
+                    yy = G.F[i].H[(y,y)]
+                    decrementReserveDegree(yy)
+
+                if u < y:
+                    a = u
                     b = y
                 else:
                     a = y
-                    b = x
+                    b = u
+                #log(HDTlog,'srp',' te', i,a,b)
                 addEdgeDF(G.F[i],a,b)
+
+                #uu = G.F[i].H[(u,u)]
+                #sanityCheck(getRoot(uu),G.F[i])
+
                 for j in range(i+1,G.maxLevel+1):
                     addEdgeDF(G.F[j],a,b,0)
+                    #uu = G.F[j].H[(u,u)]
+                    #sanityCheck(getRoot(uu),G.F[j])
                 return
+            # if (u,u) not in G.F[i].H and
+            # and len(G.F[i].nte[u]) = 0, then
+            # there is no substitute for uv in level i
             continue
         if (v,v) not in G.F[i].H:
             if len(G.F[i].nte[v]) > 0:
-                xy = G.F[i].nte[v].pop()
-                x,y = xy.val
-                if x < y:
-                    a = x
+                y = G.F[i].nte[v].pop()
+                G.F[i].nte[y].remove(v)
+
+                if len( G.F[i].nte[y] ) == 0:
+                    yy = G.F[i].H[(y,y)]
+                    decrementReserveDegree(yy)
+
+                if v < y:
+                    a = v
                     b = y
                 else:
                     a = y
-                    b = x
+                    b = v
+                #log(HDTlog,'srp',' te', i,a,b)
                 addEdgeDF(G.F[i],a,b)
+
+                #vv = G.F[i].H[(v,v)]
+                #sanityCheck(getRoot(vv),G.F[i])
+
                 for j in range(i+1,G.maxLevel+1):
                     addEdgeDF(G.F[j],a,b,0)
                 return
             continue
-        
-
-
-
 
         Tv = getRoot(G.F[i].H[(v,v)])
         Tu = getRoot(G.F[i].H[(u,u)])
@@ -185,13 +233,12 @@ def replace(G,u,v,level):
             Tu = Tv
         edgeList = getListEdgesOfLevel(Tu)
         #if len(edgeList) == 0:
-            #HDTlog.write(f'[rep] {u} edgeList empty\n')
+        #    HDTlog.write(f'[rep] {u} edgeList empty\n')
        
         # Downgrading level-i edges in Tu to level i-1
         # We know that x < y.
         for xy in edgeList:
-            x = xy.val[0]
-            y = xy.val[1]
+            x,y = xy.val
             G.level[(x,y)] = i-1
             xy.is_level = 0
             decrementLevelCountToRoot(xy)
@@ -201,17 +248,18 @@ def replace(G,u,v,level):
         # While there is an node incident reserve edges
         while Tu.reserve_degree_count and not substituted:
             #HDTlog.write(f'Tu.c {Tu.reserve_degree_count}\n')
-            xx = searchReserveNode(Tu)
-            x = xx.val[0]
+            xx = searchReserveNode(G.F[i],Tu)
+            x,_ = xx.val
+            #if G.F[i].H[(x,x)] != xx:
+            #   print("PERIGO: nó",xx.val,"não é active") 
             toBeRemoved = []
-            for y in xx.nte:
+            for y in G.F[i].nte[x]:
                 
-                # Python3 doesn't allow to modify xx.nte while iteration
+                # Python3 doesn't allow to modify G.F[i].nte[x] while iteration
                 # over it. So we add y to a 'toBeRemoved' list and will
-                # remove later.
+                # remove it later.
                 toBeRemoved.append(y)
 
-                
                 if connectedDF(G.F[i],x,y):
                     if x < y:
                         G.level[(x,y)] = i-1
@@ -220,8 +268,8 @@ def replace(G,u,v,level):
 
                     G.F[i-1].nte[x].add(y)
                     if len( G.F[i-1].nte[x] ) == 1:
-                        xx = G.F[i-1].H[(x,x)]
-                        incrementReserveDegree(xx)
+                        xx2 = G.F[i-1].H[(x,x)]
+                        incrementReserveDegree(xx2)
 
 
                     G.F[i-1].nte[y].add(x)
@@ -238,24 +286,24 @@ def replace(G,u,v,level):
                         a = y
                         b = x
                     substituted = True
-                    #log(HDTlog,'rep',' te', i-1,x,y)
+                    #log(HDTlog,'rep',' te', i,x,y)
                     addEdgeDF(G.F[i],a,b)
                     for j in range(i+1,G.maxLevel+1):
                         addEdgeDF(G.F[j],a,b,0)
                     break
 
-            # TODO: remove this call to xx
-            xx = G.F[i].H[(x,x)]
+            #HDTlog.write(f'{x} {xx.val} {len( G.F[i].nte[x] )} {toBeRemoved}\n')
             for y in toBeRemoved:
-                xx.nte.remove(y)
-                
-                yy = G.F[i].H[(y,y)]
-                yy.nte.remove(x)
-                decrementReserveDegree(yy)
+                G.F[i].nte[x].remove(y)
+                G.F[i].nte[y].remove(x)
+                if len(G.F[i].nte[y]) == 0:
+                    yy = G.F[i].H[(y,y)]
+                    decrementReserveDegree(yy)
             
-            if len(xx.nte) == 0:
+            if len( G.F[i].nte[x] ) == 0:
                 decrementReserveDegree(xx)
-
+            #sanityCheck(getRoot(xx),G.F[i])
+            #HDTlog.write(f'Tu.c {Tu.reserve_degree_count}\n')
             if substituted:
                 return
 
