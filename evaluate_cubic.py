48c514430d10fd27b20d08b0a30dd49bbf5d1a95
Arthur Rodrigues <arthurhdrodrigues@gmail.com>
2024-06-17 14:31:42 -0300

    Add dummy nodes in evaluate_cubic.py

--- evaluate_cubic.py
+++ evaluate_cubic.py
@@ -248,11 +248,24 @@ if __name__ == '__main__':
                     raise ValueError("Error in insertion")
 
                 # HDT
+                if (a,a) not in HDT_graph.F[HDT_graph.maxLevel].H:
+                    HDT_graph.F[HDT_graph.maxLevel].H[(a,a)] = dg.treapNode((a,a),0,0)
+                    a_is_placeholder = True
+                if (b,b) not in HDT_graph.F[HDT_graph.maxLevel].H:
+                    HDT_graph.F[HDT_graph.maxLevel].H[(b,b)] = dg.treapNode((b,b),0,0)
+                    b_is_placeholder = True
                 
                 start = timer()
                 root_a = dg.getRoot(HDT_graph.F[HDT_graph.maxLevel].H[(a,a)])
                 root_b = dg.getRoot(HDT_graph.F[HDT_graph.maxLevel].H[(b,b)])
                 go_to_root_HDT = timer() - start
+
+                if a_is_placeholder:
+                    a_is_placeholder = False
+                    del(HDT_graph.F[HDT_graph.maxLevel].H[(a,a)])
+                if b_is_placeholder:
+                    b_is_placeholder = False
+                    del(HDT_graph.F[HDT_graph.maxLevel].H[(b,b)])
                 
                 if root_a != root_b:
                     HDT_res.in_te_count += 1
